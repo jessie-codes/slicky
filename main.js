02c15b9b2566d109c57e7ec69fa85c6e047bde7e
Jessie Barnett <jessie.lynn.barett@gmail.com>
2016-10-16 15:54:09 -0400

    Screeps Minor
    - Deleted base harvester role, no longer needed
    - Updated auto spawner to output the role when a new creep is created
    - Cleaned up the role selector code in main

--- main.js
+++ main.js
@@ -1,41 +1,21 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-
 //Spawn Management
 const autoSpawner = require('spawner.auto');
 const clearSpawner = require('spawner.clear');
 
 //Role Management
-const spawnHarvester = require('role.harvester.spawn');
-const extensionHarvester = require('role.harvester.spawn');
+const roles = {
+	spawnHarvester: require('role.harvester.spawn'),
+	extensionHarvester: require('role.harvester.spawn'),
+	builder: require('role.builder'),
+	upgrader: require('role.upgrader')
+}
 
 module.exports.loop = function () {
 	clearSpawner.clear();	
 	autoSpawner.generate();	
 	
-	for(var name in Game.creeps) {
-		var creep = Game.creeps[name];
-		switch (creep.memory.role){
-			case 'harvester': {
-				roleHarvester.run(creep);
-				break;
-			}
-			case 'upgrader': {
-				roleUpgrader.run(creep);
-				break;
-			}
-			case 'builder': {
-				roleBuilder.run(creep);
-				break;
-			}
-			case 'extensionHarvester': {
-				extensionHarvester.run(creep);
-				break;
-			}
-			default: {
-				spawnHarvester.run(creep);
-			}
-		}
+	for(const name in Game.creeps) {
+		const creep = Game.creeps[name];
+		roles[creep.memory.role].run(creep);
 	}
 }
--- spawner.auto.js
+++ spawner.auto.js
@@ -6,7 +6,7 @@ const spawn = {
 			const creepers = _.filter(Game.creeps, (creep) => creep.memory.role == n);
 			if (creepers.length < roles[n].min) {
 				const name = Game.spawns['Spawner'].createCreep(roles[n].body, undefined, {role: n});
-				console.log(`Created ${name} with role {$n}`);
+				console.log(`Created ${name} with role ${n}`);
 			}
 		}
 	}
